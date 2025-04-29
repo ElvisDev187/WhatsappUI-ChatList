106455b0ab7ecd0dca3709ed8d0808daf22572ef
ElvisDev187 <cabrelelvis187@gmail.com>
2025-04-29 10:04:52 +0100

    setup svg import

---
+++ components/header/action.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import More from '../../assets/more.svg';
+const Action = () => {
+  return (
+    <View>
+      <Text>Action</Text>
+      <More width={20} height={20} />
+    </View>
+  );
+};
+
+export default Action;
+
+const styles = StyleSheet.create({});
